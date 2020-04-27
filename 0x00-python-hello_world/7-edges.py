f0211e9365ab14eadc93f96bf9159226effc822a
exploitpnk <1245@holbertonschool.com>
2020-04-27 12:45:26 -0500

    [Update] fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[7:9]
+word_last_2 = word[-2:]
 middle_word = word[1:8]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
