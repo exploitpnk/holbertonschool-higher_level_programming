7ba235d12c0b535511bf61c6098b80f777acc8ef
exploitpnk <exploitpnk@gmail.com>
2020-04-28 21:59:58 -0500

    [Add] comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for y in range(0, 10):
+        if (x < y):
+            print("{:d}{:d}".format(x, y), end="")
+            if x < 8:
+                print(",", end="")
+print("")
