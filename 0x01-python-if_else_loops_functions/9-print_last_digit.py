6da67820e04f4a1034385aa8904b4e3ef38ed08a
exploitpnk <exploitpnk@gmail.com>
2020-04-28 23:09:42 -0500

    [Add] last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-	if number < 0:
-		number = number * -1
-	lastdigit = number % 10
-	print("{:d}".format(lastdigit), end="")
-	return(lastdigit)
+    if number < 0:
+        number = number * -1
+    lastdigit = number % 10
+    print("{:d}".format(lastdigit), end="")
+    return(lastdigit)
