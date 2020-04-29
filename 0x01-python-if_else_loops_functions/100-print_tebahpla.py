a1518818f44040617d52e757d0a060581b875ccc
exploitpnk <exploitpnk@gmail.com>
2020-04-29 00:01:36 -0500

    [Add] reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for character in range(122, 96, -1):
-	print("{}".format(chr(character)), end="") if (character % 2 == 0) else print("{}".format(chr(character - 32)), end="")
+    if (character % 2 == 0):
+        print("{}".format(chr(character)), end="")
+    else:
+        print("{}".format(chr(character - 32)), end="")
