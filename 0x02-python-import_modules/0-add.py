88390a38b04e547b096937c616ba7dadcc24fa92
exploitpnk <exploitpnk@gmail.com>
2020-04-30 11:42:26 -0500

    [Add] add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import add_0
+
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,6 @@
+## 0x02. Python - import & modules
+
+
+## Tasks
+
+* [Import a simple function from a simple file]()
