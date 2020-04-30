ba95f65de07c159a647d05af0ea5fb04e7d09030
exploitpnk <exploitpnk@gmail.com>
2020-04-30 13:00:05 -0500

    [Fix] imports

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+from add_0 import add
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
