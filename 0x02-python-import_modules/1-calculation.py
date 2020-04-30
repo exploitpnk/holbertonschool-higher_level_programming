20da7bd093cf6e0705a2b9d2853e9a4b2bf072b4
exploitpnk <exploitpnk@gmail.com>
2020-04-30 12:53:17 -0500

    [Fix] pep8

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-	import calculator_1
+    import calculator_1
 
-	a = 10
-	b = 5
+    a = 10
+    b = 5
 
-	print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-	print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
-	print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
-	print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
