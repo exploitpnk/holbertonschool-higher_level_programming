7e5e15c917da0e7d507efbeb0f173125c9c570d5
exploitpnk <exploitpnk@gmail.com>
2020-04-30 13:15:45 -0500

    [Add] hex test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == '__main__':
     sys.argv.pop(0)
-    total = 0
+    total = hex(0x00)
     for number in sys.argv:
         total += int(number)
     print("{}".format(total))
