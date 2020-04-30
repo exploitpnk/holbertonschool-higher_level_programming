0e7f0d896afe83059559ab615643cbd8eb36de87
exploitpnk <exploitpnk@gmail.com>
2020-04-30 12:56:35 -0500

    [Fix] imports

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import calculator_1
-
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
     print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
