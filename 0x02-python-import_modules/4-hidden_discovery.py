94b9ecdb00d5ed8e7f9025b34ca34d9726a92421
exploitpnk <exploitpnk@gmail.com>
2020-04-30 20:24:25 -0500

    [Add] hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ import hidden_4
 if __name__ == '__main__':
     data = dir(hidden_4)
     for item in data:
-        if (data[:2]) != '__':
+        if (item[:2]) != '__':
             print("{}".format(item))
