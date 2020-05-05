2adbe27e8bf44fc4184ed609288da9d1a693843a
exploitpnk <exploitpnk@gmail.com>
2020-05-05 16:20:39 -0500

    [Fix] element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     listlength = len(my_list)
     if (idx < 0 or idx > listlength):
-        return (none)
+        return (None)
     else:
         return (my_list[idx])
