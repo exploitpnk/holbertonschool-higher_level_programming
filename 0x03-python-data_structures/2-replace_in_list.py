4ca16c4e35f35a875f0813eeff7df13b4c1a5e34
exploitpnk <exploitpnk@gmail.com>
2020-05-05 04:53:13 -0500

    [Add] replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    listlength = len(my_list)
+    if (idx < 0 or idx > listlength):
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
