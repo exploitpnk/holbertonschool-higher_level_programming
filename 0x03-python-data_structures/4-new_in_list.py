854709e948040de2947793cd0af5cabaa61e5925
exploitpnk <exploitpnk@gmail.com>
2020-05-05 16:37:50 -0500

    [Add] new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     listlength = len(my_list) - 1
     if (idx < 0 or idx > listlength):
-        return (my_list[:])
+        return (my_list)
     else:
         new_list = my_list[:]
         new_list[idx] = element
