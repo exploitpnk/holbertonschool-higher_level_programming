6735926d6ad3dd616885d292486dc56cefa7f5ca
exploitpnk <exploitpnk@gmail.com>
2020-05-05 04:44:56 -0500

    [Add] print specific item

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    listlength = len(my_list)
+    if (idx < 0 or idx > listlength):
+        return (none)
+    else:
+        return (my_list[idx])
