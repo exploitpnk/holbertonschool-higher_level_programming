1a9db1fc7de4c278602a2c132f6ea38cdfe93a6b
exploitpnk <exploitpnk@gmail.com>
2020-05-05 16:30:00 -0500

    [Fix] conditional added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for number in my_list:
-        print("{:d}".format(number))
+    if my_list:
+        my_list.reverse()
+        for number in my_list:
+            print("{:d}".format(number))
