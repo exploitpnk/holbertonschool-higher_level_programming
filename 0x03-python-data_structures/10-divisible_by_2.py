441848872f6bbe57be8387bbe1f81e4e19b05ca6
exploitpnk <exploitpnk@gmail.com>
2020-05-05 22:10:18 -0500

    [Fix] append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list[:]
+    new_list = []
     for number in my_list:
         if (number % 2) == 0:
-            new_list[number] = True
+            new_list.append(True)
         else:
-            new_list[number] = False
+            new_list.append(False)
     return(new_list)
