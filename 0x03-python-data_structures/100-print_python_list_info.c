ced0cc47302a03d6c6b43f3097b18856fef18a86
exploitpnk <exploitpnk@gmail.com>
2020-05-05 23:23:09 -0500

    [Fix] quotes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -16,12 +16,12 @@ void print_python_list_info(PyObject *p)
 	list_size = PyList_Size(p);
 	a = ((PyListObject *)p)->allocated;
 
-	printf('[*] Size of the Python List = %ld\n', list_size);
-	printf('[*] Allocated = %ld\n', a);
+	printf("[*] Size of the Python List = %ld\n", list_size);
+	printf("[*] Allocated = %ld\n", a);
 	while (i < list_size)
 	{
 		obj = PyList_GetItem(p, i);
-		printf('Element %ld: %s\n', i, Py_TYPE(obj)->tp_name);
+		printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
 		i++;
 	}
 }
