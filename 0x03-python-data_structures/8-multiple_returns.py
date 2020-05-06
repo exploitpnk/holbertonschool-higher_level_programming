00c1a3a0a1483d61a83d9f9249aa7d0bf3c04519
exploitpnk <exploitpnk@gmail.com>
2020-05-05 21:25:29 -0500

    [Fix] conditional

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         my_tuple = (0, None)
     else:
-        my_tuple = (len(sentence), sentence[:1])
-        return(my_tuple)
+        return(len(sentence), sentence[:1])
