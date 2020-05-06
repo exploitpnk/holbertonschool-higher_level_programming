6f10f83290e16b850fac7e037366bcd315b85a32
exploitpnk <exploitpnk@gmail.com>
2020-05-05 23:15:14 -0500

    [Add] python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info - shows info about Python lists
+ * @p: object
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+void print_python_list_info(PyObject *p)
+{
+	int a, i;
+	long int list_size;
+
+	i = 0;
+	PyObject *obj;
+
+	list_size = PyList_Size(p);
+	a = ((PyListObject *)p)->allocated;
+
+	printf('[*] Size of the Python List = %ld\n', list_size);
+	printf('[*] Allocated = %ld\n', a);
+	while (i < list_size)
+	{
+		obj = PyList_GetItem(p, i);
+		printf('Element %ld: %s\n', i, Py_TYPE(obj)->tp_name);
+		i++;
+	}
+}
