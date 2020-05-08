3869248cd4c515c90960342b4bdf3b8ad6548966
exploitpnk <exploitpnk@gmail.com>
2020-05-07 20:25:11 -0500

    [Add] update dic

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary.keys():
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return(a_dictionary)
