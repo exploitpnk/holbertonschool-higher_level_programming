b917ac363161ffab8356e9dbaa6fd0605a638a5d
exploitpnk <exploitpnk@gmail.com>
2020-05-18 19:59:35 -0500

    [Add] safe print list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     printed_values = 0
     try:
-        for item in range(0, x):
-            if isinstance(my_list[item], int):
-                print("{:d}".format(my_list[item]), end='')
+        for i in range(0, x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
                 printed_values += 1
-        print('')
-        return(item)
+        print("")
+        return (printed_values)
     except TypeError:
-        print('')
+        print("")
