755ef7b8e59d0a7c4e4aea22a54399ed722fca0e
exploitpnk <exploitpnk@gmail.com>
2020-05-18 19:45:38 -0500

    [Add] safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-    except TypeError:
-        print("Only numbers are allowed")
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
