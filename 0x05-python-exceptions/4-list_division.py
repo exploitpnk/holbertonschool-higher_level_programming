05a0300bdf1c9dafc10ec18f6954bc1bf6549bd3
exploitpnk <exploitpnk@gmail.com>
2020-05-18 20:29:58 -0500

    [Add] list divistion

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    value = 0
+    for i in range(list_length):
+        try:
+            value = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(value)
+            value = 0
+    return(new_list)
