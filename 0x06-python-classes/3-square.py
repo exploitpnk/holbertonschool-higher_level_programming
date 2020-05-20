ccca1b33fd4ab9f33b12a4b39f908046b5a2ad78
exploitpnk <exploitpnk@gmail.com>
2020-05-19 21:51:53 -0500

    [Add] calculate area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class Square that defines a square """
+
+
+class Square:
+    """ Class Square """
+    def __init__(self, size=0):
+        """ Constructor """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculatre square area """
+        return(self.__size * self.__size)
