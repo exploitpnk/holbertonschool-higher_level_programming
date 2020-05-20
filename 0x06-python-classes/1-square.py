3f17f8b259eea59a54cb3a75d6fe107cd8b6b38d
exploitpnk <exploitpnk@gmail.com>
2020-05-19 19:22:23 -0500

    [Fix] Docstring added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Write a class Square that defines a square """
+
+
 class Square:
-	def __init__(self, size):
-		self.__size = size
+    """ Class Square """
+    def __init__(self, size):
+        self.__size = size
