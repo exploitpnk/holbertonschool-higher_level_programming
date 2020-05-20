04dbd1f116b9c8918cbaf7705d67019ec050c2b6
exploitpnk <exploitpnk@gmail.com>
2020-05-19 22:03:19 -0500

    [Add] getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Class Square that defines a square """
+
+
+class Square:
+    """ Class Square """
+    def __init__(self, size=0):
+        """ Constructor """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Calculatre square area """
+        return(self.__size * self.__size)
