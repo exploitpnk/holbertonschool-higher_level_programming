0555f4b09c0a8e28c1885ddd6929b9bf9cef7113
exploitpnk <exploitpnk@gmail.com>
2020-05-19 21:41:13 -0500

    [Add] define square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,13 +5,10 @@
 class Square:
     """ Class Square """
     def __init__(self, size=0):
-        """ __init__ method """
-        self.__size = size
-        try:
-            int(self.__size)
-            if self.__size >= 0:
-                pass
-            else:
-                raise ValueError("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+        """ Constructor """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
