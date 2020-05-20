c4bf1f9e5bff3c721afbcc0737c7e571b73a9be2
exploitpnk <exploitpnk@gmail.com>
2020-05-19 21:37:30 -0500

    [Add] define square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+""" Class Square that defines a square """
+
 
 class Square:
-	def __init__(self, size=0):
-		self.__size = size
-		try:
-			int(self.__size)
-			if self.__size >= 0:
-				pass
-			else:
-				raise ValueError("size must be >= 0") 
-		except TypeError:
-			print("size must be an integer")
+    """ Class Square """
+    def __init__(self, size=0):
+        """ __init__ method """
+        self.__size = size
+        try:
+            int(self.__size)
+            if self.__size >= 0:
+                pass
+            else:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
