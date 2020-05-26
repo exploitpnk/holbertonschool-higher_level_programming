0cd3ee9e5cb0a1cb50b205338040b36afa9158d4
exploitpnk <exploitpnk@gmail.com>
2020-05-25 21:10:41 -0500

    [Add] rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Class Rectangle """
+
+
+class Rectangle:
+    """ Empty class Rectangle that defines a rectangle """
+    def __init__(self, width=0, height=0):
+        """ method is executed immediately after create an object """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ Getter """
+        return self.__heigth
+
+    @height.setter
+    def height(self, value):
+        """ Setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__heigth = value
