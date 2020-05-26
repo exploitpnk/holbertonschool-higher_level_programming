3e6c8a5c8be1b10384845ca4607d177ab29eecc5
exploitpnk <exploitpnk@gmail.com>
2020-05-26 02:39:41 -0500

    [Fix] bigger than

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -83,5 +83,5 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
