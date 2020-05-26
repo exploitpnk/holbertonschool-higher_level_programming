eaa703f9bda8ab927b0fc58a154b5c0cbb6391e3
exploitpnk <exploitpnk@gmail.com>
2020-05-26 00:36:30 -0500

    [Fix] output corrected

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -64,4 +64,4 @@ class Rectangle:
 
     def __repr__(self):
         """ return object representation """
-        return("Rectangle({}{})".format(self.__width, self.__height))
+        return("Rectangle({:d}, {:d})".format(self.__width, self.__height))
