28143e919173481b0ee77fb8ba8e98377266cea5
exploitpnk <exploitpnk@gmail.com>
2020-05-26 03:09:30 -0500

    [Add] rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,6 +85,8 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
+        else:
+            rect_2
 
     @classmethod
     def square(cls, size=0):
