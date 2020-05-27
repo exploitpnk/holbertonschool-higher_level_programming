a55631ba65b5d9cb041213b1794553e02c65eb3c
exploitpnk <exploitpnk@gmail.com>
2020-05-27 05:40:49 -0500

    [Update] Update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(h=[]):
-    h.append('Holberton')
-    return (', '.join(h))
+def magic_string(my_list=[]):
+    my_list.append('Holberton')
+    return (', '.join(my_list))
