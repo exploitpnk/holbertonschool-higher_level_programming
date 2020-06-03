5b882539d85efe3960e5ee04704f34f3fa7fd613
exploitpnk <exploitpnk@gmail.com>
2020-06-03 10:30:43 -0500

    [Add] append write

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open('file_append.txt', 'a', encoding='utf-8') as f:
+        return(f.write(text))
