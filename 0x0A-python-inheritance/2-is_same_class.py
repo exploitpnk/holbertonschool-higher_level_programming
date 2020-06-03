0fe17cf88745ffa3802c64d4df34f939e459654e
exploitpnk <exploitpnk@gmail.com>
2020-06-02 20:20:46 -0500

    [Add] is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check if an object is an instance of a specific class """
+
+
+def is_same_class(obj, a_class):
+    """ Check if an object is an instance of a specific class """
+    return(isinstance(obj, a_class))
