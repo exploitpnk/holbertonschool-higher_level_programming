265b31f6b8e58ab99abdebfc36f3a0ee8eaf2164
exploitpnk <exploitpnk@gmail.com>
2020-06-02 20:33:34 -0500

    [Fix] pycodestyle

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+returns True if the object is an instance of, or if the object is an
+instance of a class that inherited from, the specified class ;
+otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class ;
+    otherwise False.
+    """
+    return isinstance(obj, a_class)
