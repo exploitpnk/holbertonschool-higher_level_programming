dfb05b6bf356797e9d8fd9e1475329ee53ee9311
exploitpnk <exploitpnk@gmail.com>
2020-06-02 21:17:30 -0500

    [Add] base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    def area(self):
+        """ method area """
+        raise Exception("area() is not implemented")
