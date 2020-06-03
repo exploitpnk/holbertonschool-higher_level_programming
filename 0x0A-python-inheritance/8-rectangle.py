ab582261933a3e9e322cb6c231d74a3ca12366f5
exploitpnk <exploitpnk@gmail.com>
2020-06-02 21:48:20 -0500

    [Add] rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" Class Rectangle """
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle """
+    def __init__(self, width, height):
+        """ init method """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
