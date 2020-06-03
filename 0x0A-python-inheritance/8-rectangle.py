181a74e252d637f8371b0b35efb3241ad2ceb07c
exploitpnk <exploitpnk@gmail.com>
2020-06-02 23:29:09 -0500

    [Update]

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ Class Rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
