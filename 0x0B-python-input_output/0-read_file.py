2631af5646395a32d992ee55d0711567d06d4eeb
exploitpnk <exploitpnk@gmail.com>
2020-06-03 06:28:03 -0500

    [Add] read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout """
+    with open('my_file_0.txt') as f:
+        print(f.read())
