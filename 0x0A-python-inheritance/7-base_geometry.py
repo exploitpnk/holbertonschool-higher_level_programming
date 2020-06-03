b7241b3433be6fe46d6cd52f3cbbb74555bec344
exploitpnk <exploitpnk@gmail.com>
2020-06-02 21:29:08 -0500

    [Add] basegeometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    def area(self):
+        """ Method area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Method integer validator """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
