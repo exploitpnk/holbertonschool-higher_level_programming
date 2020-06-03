06528262a8749f7e656522f3b504a76b19ff3901
exploitpnk <exploitpnk@gmail.com>
2020-06-02 23:27:24 -0500

    [Fix] type()

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ Method integer validator """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
