630d322478eafdf0d13196f53000c4eee375c55c
exploitpnk <exploitpnk@gmail.com>
2020-06-02 20:02:45 -0500

    [Add] inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" inherits from list """
+
+
+class MyList(list):
+    """ inherits from list """
+
+    def print_sorted(self):
+        """ print sorted list """
+        print(sorted(self))
