16b0ef8691da7f86d2532d6f14a48e8a493fe54a
exploitpnk <exploitpnk@gmail.com>
2020-06-03 10:25:53 -0500

    [Add] write file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+writes a string to a text file (UTF8) and returns the number of
+characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8) and returns the number of
+    characters written
+    """
+    with open('my_first_file.txt', 'w+', encoding='utf-8') as f:
+        return(f.write(text))
