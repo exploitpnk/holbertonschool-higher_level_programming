6aea1b601870d3e2b71ac9dc0deb8c4fcbb45237
exploitpnk <exploitpnk@gmail.com>
2020-06-03 10:42:21 -0500

    [Add] serialize

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" returns the JSON representation of an object (string) """
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object (string) """
+    return json.dumps(my_obj)
