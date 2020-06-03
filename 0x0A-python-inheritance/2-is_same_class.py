40b617bf4fdcc6b3961a3fb2e06ca1647adb6ab9
exploitpnk <exploitpnk@gmail.com>
2020-06-02 20:25:12 -0500

    [Fix] pycodestyle

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-""" Check if an object is an instance of a specific class """
+"""
+returns True if the object is exactly an instance of the specified class ;
+otherwise False.
+"""
 
 
 def is_same_class(obj, a_class):
-    """ Check if an object is an instance of a specific class """
-    return(isinstance(obj, a_class))
+    """
+    returns True if the object is exactly an instance of the specified class ;
+    otherwise False.
+    """
+    return(type(obj) == a_class)
