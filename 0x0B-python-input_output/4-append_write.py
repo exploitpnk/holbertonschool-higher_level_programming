7c89c4d8b21e092320bd0f29a8e7fcb95804dfa5
exploitpnk <exploitpnk@gmail.com>
2020-06-04 05:46:17 -0500

    [Fix] filename

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
     appends a string at the end of a text file (UTF8)
     and returns the number of characters added
     """
-    with open('file_append.txt', 'a', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         return(f.write(text))
