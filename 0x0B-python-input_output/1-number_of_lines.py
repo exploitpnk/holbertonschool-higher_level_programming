b0cf711f421309145444c5da9fca9ef9a325fc58
exploitpnk <exploitpnk@gmail.com>
2020-06-04 05:44:49 -0500

    [Fix] filename

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,7 +4,7 @@
 
 def number_of_lines(filename=""):
     """ returns the number of lines of a text file """
-    with open('my_file_0.txt', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         lines = 0
         for line in f:
             lines += 1
