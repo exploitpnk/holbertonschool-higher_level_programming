11a10618e26d11d5d4424599518bd176b05816c2
exploitpnk <exploitpnk@gmail.com>
2020-06-04 05:57:22 -0500

    [Fix] utf8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
-    with open(filename encoding='uft-8') as f:
+    with open(filename, encoding='utf8') as f:
         print(f.read(), end='')
