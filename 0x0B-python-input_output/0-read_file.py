12ce749ed5ac0545cc4199e1f481d48df8f1b5cd
exploitpnk <exploitpnk@gmail.com>
2020-06-04 05:44:24 -0500

    [Fix] filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
-    with open('my_file_0.txt', encoding='uft-8') as f:
+    with open(filename encoding='uft-8') as f:
         print(f.read())
