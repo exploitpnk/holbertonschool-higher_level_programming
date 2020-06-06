a33b66b29cb9344d7af41cd566decd426e716581
exploitpnk <exploitpnk@gmail.com>
2020-06-06 08:31:21 -0500

    [Fix] print lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,24 +2,13 @@
 """ reads n lines of a text file (UTF8) and prints it to stdout """
 
 
-def number_of_lines(filename=""):
-    """ returns the number of lines of a text file """
-    with open(filename, encoding='utf8') as f:
-        lines = 0
-        for line in f:
-            lines += 1
-        return lines
-
-
 def read_lines(filename="", nb_lines=0):
     """ reads n lines of a text file (UTF8) and prints it to stdout """
     with open(filename, encoding='utf8') as f:
-        total_lines = number_of_lines(filename)
-        if nb_lines <= 0 or nb_lines >= total_lines:
-            print(f.read())
+        lines = f.readlines()
+        if nb_lines <= 0 or nb_lines >= len(lines):
+            for i in lines:
+                print(i, end='')
         else:
-            printed_lines = 0
-            for line in f:
-                if printed_lines < nb_lines:
-                    print(line, end='')
-                    printed_lines = printed_lines + 1
+            for i in range(0, nb_lines):
+                print(lines[i], end='')
