00eb28bdad816e248d44c95178de50cd43b1f79a
exploitpnk <exploitpnk@gmail.com>
2020-06-06 19:10:00 -0500

    [Update] append changed to append

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ writes an Object to a text file, using a JSON representation """
-    with open(filename, 'a', encoding='utf8') as f:
+    with open(filename, 'w', encoding='utf8') as f:
         json.dump(my_obj, f)
