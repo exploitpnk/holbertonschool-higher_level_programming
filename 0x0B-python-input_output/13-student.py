d6d3f04952315769db064e97119c21cc9d71bed3
exploitpnk <exploitpnk@gmail.com>
2020-06-07 04:08:17 -0500

    [Add] student

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" class Student that defines a student """
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+class Student:
+    """ class Student that defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ init method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if type(attrs) == list:
+            new_dic = {}
+            for attribute in attrs:
+                if attribute in self.__dict__:
+                    new_dic[attribute] = self.__dict__[attribute]
+            return new_dic
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+        for item in json:
+            self.__dict__[item] = json[item]
