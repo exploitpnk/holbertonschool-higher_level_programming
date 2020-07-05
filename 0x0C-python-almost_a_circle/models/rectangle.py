356f07da10cfffc88577c5662fe722013b7d6d83
exploitpnk <exploitpnk@gmail.com>
2020-07-05 02:08:49 -0500

    [Add] rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,3 +33,23 @@ class Rectangle(Base):
     def height(self, value):
         """ setter """
         self.__height = value
+    
+    @property
+    def x(self):
+        """ getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setter for x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ setter for y """
+        self.__y = value
