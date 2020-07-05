9c17dcaa355d704e4b69bdd04a72d6bba2510ebb
exploitpnk <exploitpnk@gmail.com>
2020-07-05 03:15:02 -0500

    [Fix] space

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,7 @@ class Rectangle(Base):
         for i in range(self.y):
             print("")
         for j in range(self.height):
-            print("" * self.x, end='')
+            print(" " * self.x, end='')
             print("#" * self.width)
 
     def __str__(self):
