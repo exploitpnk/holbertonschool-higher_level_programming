a916fda8835bbe3c929bdcc494b5fbf54af4afd6
exploitpnk <exploitpnk@gmail.com>
2020-07-13 22:26:13 -0500

    [Add] table with multiples rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- Create a table second_table with multiples rows
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) values(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
