486259fb8b03a5e59eb90cbd91bf4ea3e610e109
exploitpnk <exploitpnk@gmail.com>
2020-09-08 22:11:42 -0500

    [Add] SELECT states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
