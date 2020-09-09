22d0499fa7217e177dc76cc25b773a7ca865db82
exploitpnk <exploitpnk@gmail.com>
2020-09-08 23:28:01 -0500

    [Add] filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,11 +16,9 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-    cur.execute(
-        "SELECT id, name FROM states WHERE name "
-        "COLLATE latin1_general_cs"
-        "LIKE 'N%' ORDER BY id ASC;"
-        )
+    cur.execute("SELECT id, name FROM states WHERE name "
+                "COLLATE latin1_general_cs"
+                "LIKE 'N%' ORDER BY id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
