d2dc90de28810d92b99b05657199168f2f958e80
exploitpnk <exploitpnk@gmail.com>
2020-09-09 01:55:10 -0500

    [Add] cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,8 +15,11 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM cities ORDER BY id ASC;")
+    sql = """
+    SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+    """
+    cur.execute(sql)
 
     rows = cur.fetchall()
     for row in rows:
