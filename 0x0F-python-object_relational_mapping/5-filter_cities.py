7bd8a00d4d037ceef24dea18cf511730ba03d40c
exploitpnk <exploitpnk@gmail.com>
2020-10-30 00:53:35 -0500

    [Add] 5 filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""
-    Lists all cities of that state, using the database hbtn_0e_4_usa
-"""
+""" All cities by state """
 
-
-import MySQLdb
 from sys import argv
-
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
     cur = db.cursor()
 
-    sql = """
-    SELECT cities.name FROM cities JOIN states ON
-    cities.state_id = states.id WHERE states.name = %s
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
     ORDER BY cities.id ASC;
     """
 
-    cur.execute(sql, (argv[4],))
+    cur.execute(query, (state_name,))
 
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    rows = [i[0] for i in rows]
+
+    print(', '.join(rows))
+
     cur.close()
     db.close()
