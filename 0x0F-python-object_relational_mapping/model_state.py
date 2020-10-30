8dfd3911a384ce290fee4e7f1397a5237bcd5c2a
exploitpnk <exploitpnk@gmail.com>
2020-10-30 01:02:28 -0500

    [Add] model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Start link class to table in database """
+
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
