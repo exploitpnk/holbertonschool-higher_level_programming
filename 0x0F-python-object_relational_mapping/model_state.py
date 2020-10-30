67bc97c56c1dd89ed779d8d698d10bd2d0bc33ac
exploitpnk <exploitpnk@gmail.com>
2020-10-30 01:06:36 -0500

    [Add] model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+"""Firt state model"""
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class of states table """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
