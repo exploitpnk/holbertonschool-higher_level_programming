2a31fe4b1663554dde436575849078a0ffdd0ca9
exploitpnk <exploitpnk@gmail.com>
2020-11-05 18:23:09 -0500

    [Fix] semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -10,7 +10,7 @@ You must use the module request
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function(err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
