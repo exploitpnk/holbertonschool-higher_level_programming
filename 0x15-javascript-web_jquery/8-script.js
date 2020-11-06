4c6de31ca956e95037041df951adfd62699d56fa
Gabriel Montaño <57080920+exploitpnk@users.noreply.github.com>
2020-11-06 12:15:13 -0500

    Delete 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,5 +0,0 @@
-$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (response) {
-  response.results.forEach(function (film) {
-    $('#list_movies').append('<li>' + film.title + '</li>');
-  });
-});
