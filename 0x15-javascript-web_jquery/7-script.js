e0c5d50fdce5a1ab54c6dc148534ee9703d31862
Gabriel Montaño <57080920+exploitpnk@users.noreply.github.com>
2020-11-06 12:15:21 -0500

    Delete 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data, status) => {
-    if (status === 'success') {
-      $('div#character').text(data.name);
-    }
-  });
-});
